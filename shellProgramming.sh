54574e11ae7636ae17ecb50cd03d734696f04dc2
a-raj <seriouswithavi@gmail.com>
2018-12-10 01:49:36 +0530

    improvemets

--- shellProgramming.sh
+++ shellProgramming.sh
@@ -1,3 +1,153 @@
+
+# 1. cal command
+# to view the calander of a particular month
+cal 6 2009
+cal feb 2033
+
+
+# 2. date command
+date
+
+# to view it in a particular format %m for month and %y for year %n for new line 
+date '+DATE: %m-%y %nTIME: %H:%M:%S'
+
+
+# 3. touch command it is use to crate a empty file, below 3 files will be created 
+touch file1 file2 file3
+
+
+# 4. mkdir: to make a directory or folder
+mkdir test 
+mkdir Documents/Test_Folder
+
+
+# 5. cd: change directory 
+
+
+# 6. cat: 
+cat < file_name # take the input from the file and show it in my screen
+cat > file_name # take the input from the console and save it in the file name file_name
+cat file_name # search a file with file name and show it
+cat file_name_1 file_name_2 > file3 # to mearage two files
+# cltr + D: End of document
+
+
+# 7. mv: rename a file
+mv file_name_old file_name_new
+
+
+# 8. rm: remove
+rm file_name # remove file
+rm -r directory_name # remove directory
+rmdir directory_name # remove directory
+
+
+# 9. cp: copy file from one loaction to another
+cp file_name directory_name 
+
+
+# 10. mv: move file form one location to another
+mv file_name directory_name 
+
+
+# 11. links 
+# hard and soft
+ln file_name another_file_name 
+# it will create a hard link which is a replica of a file(file_name) i.e. changes in one file will refelect in another file & wont be deleted if one is deleted  also it will occupied the same space as the root file or dir
+
+ln -s file_name link_name # this will create the soft link of the original file (shortcuts) 
+
+
+# 12. File permission rwx in binary 111 mean 7 in decimal owner group and other so 777
+# read (4),  write (2) and execute (1) all have ceratin numeric values : total numeric value will be 7
+
+
+# 13. unmask: 4 ditig number, when a file is created what unix does is it substract the umask from value 666 for a file and 777 for a dir
+
+
+# 14. ls: tell me all the files and folder in the home dir
+ls directory_name # give the list of files and folder 
+ls -l # l mean long listing give the details of the files
+ls -a # l mean all, show all the files and folder in the dir also hiden one
+
+
+# 15. chmode: change file or dir permission
+chmode 777
+
+
+# 16. uname -a
+# -a mean all, show what is your kernal and processors are
+
+
+# 17. file * : It tells about what type of file you have ie file encoding and type
+
+
+# 18. wc: word count 
+wc fileName # first it tells about number of line, 2nd no of words and 3rd the number of alphabets in your test file.
+wc -l file_name # just print the line
+wc -w file_name # number of words
+wc -c file_name # number of characters
+
+
+# 19. sort
+sort file_name # it will show the alphabetcal sorted o/p of file
+sort # then it will wait for the input from the consol, which it will sort
+
+
+# 20. cut: used for filtering text
+cut # default it will treat tab as a delimeter
+cut -d"-" -f 1,3 file_name # -d mean delimeter and then provide the delimeter -f to tell extract the fields then provide the fiels number that after seperating delimeter
+
+######### e.g. ###########
+$ cat players
+Name-Sport-Age
+Roger-Tennis-30
+Ragel Naddal-Tennis-25
+Tiger Woods-Golf-37
+Michael Phelps-Swimmer-27
+Kobe-Basketvball-34
+
+$ cut -d"-" -f 1,3 players
+Name-Age
+Roger-30
+Ragel Naddal-25
+Tiger Woods-37
+Michael Phelps-27
+Kobe-34
+########## e.g. end ########
+
+
+# 21. dd: used for conversion
+dd if=inputFileName of=OutputFileName conv=ucase # if mean input file, op mean output file, conv mean convert, ucase mean upper case
+# also use to format pendrives
+
+dd if=inputFileName of=OutputFileName conv=ebcdic # convertng to european character encoding format
+# use file * to check the format then we get  Non-ISO extended-ASCII text, with NEL line terminators
+
+
+# 22. man: short for manual command
+# man is the help repository for the command in unix
+man command_name # it will open all manual for the command
+
+
+# 23. bannder: 
+banner "bye bye" # this will display 10 char in a line 
+banner bye bye # this will splits the bye bye and shows it
+
+
+# 24. compress: 
+compress -v file_name # -v is for shell to show the %age of compression
+
+
+# 25. uncompress: To decompress
+uncompress file_name.z
+
+
+# 26. zcat: to see the compress file without uncompressing it
+zcat file_name.z
+
+# --------------------------------------------------------
+
 # $0 - The filename of the current script.|
 # $n - The Nth argument passed to script was invoked or function was called.|
 # $# - The number of argument passed to script or function.|
@@ -30,132 +180,18 @@ filename="sample.md"
 if [ -e "$filename" ]; then
     echo "$filename exists as a file"
 fi
-############################################
 
-$ date '+DATE:%m-%y%nTIME:%H:%M:%S'
-# O/P
-# DATE:11-17
-# TIME:20:13:36
+# --------------------------------------------
 
 
-# touch fileName : it creates the file with the given file name
-
-# mkdir : creates a new directory
-
-# cd : change directory
-
-# cat > test :  create acat file and opens it to write
-
-# Cltr+D is end of document 
-
-# cat < test : this will open the file and show whats in it
-
-# cat fileName1 fileName2 > mergedFileName : two merge files merge into one
-
-# mv fileName NewFileName : rename the file
-
-# rm fileName : remove the file, also capable of removing directory
-
-# rm -r directoryName :  remove directoty 
-# rmdir : same as rm -r 
-
-# cp fileName dirAddress : copy file to location
-
-# Linkes : 2 kind 
-# soft one and hard one
-
-# ln fileName anotherFile : it will create a hard link that is a replica of a file(fileName) i.e changes in one file will refelect in another file wont be deleted if one is deleted
-
-# ln -s old old_soft : this will create the soft link of the original file (kind of shortcuts) 
-
-
-# permissions read (4),  wite (2) and execute (1) all have ceratin numeric values : total numeric value will be 7
-
-# umask : 4 ditig number, when a file is created what unix does is it substract the umask from value 666 for a file and 777 for a dir
-
-# ls -l : long listing, shows file along with its permissionsions
-# - indicates the normal file 
-# d indicates the directory
-# file owner groups, and others i.e three group of read write and execute
-
-# ls -a shows all the files that is system file also
-
-# chmode 777 fileName : 4  for read 2 for write and 1 for excute this will grand owner, group and other permissions for the rwx for all 3
-
-# uname -a : this will give information about the system -a stand for the all the informaton
-
-##############################
-
-# wc command 
-# file * : all the files in the dir :  all the contents of home folder
-It tells about what type of file you have ie file encoding and type
-
-# wc fileName : first it tells about number of line, 2nd no of words and 3rd the number of alphabets in your test file.
-# Wc -l : just print the line
--w : number of words
--c :  number of characters
-
-
-### Short command
-sort fileName : it will show the alphabetcal shorted o/p of file
-sort : then it will wait for the input for the which it will sort
-
-### cut command
-cut : default it will treat tab as a delimeter
-cut -d"-" -f 1,3 fileName : -d mean delimeter and then provide the delimeter -f to tell extract the fields then provide the fiels number that after seperating delimeter
-
-$ cat players
-Name-Sport-Age
-Roger-Tennis-30
-Ragel Naddal-Tennis-25
-Tiger Woods-Golf-37
-Michael Phelps-Swimmer-27
-Kobe-Basketvball-34
-
-$ cut -d"-" -f 1,3 players
-Name-Age
-Roger-30
-Ragel Naddal-25
-Tiger Woods-37
-Michael Phelps-27
-Kobe-34
-
-########## dd command
-used for conversion
-dd if=inputFileName of=OutputFileName conv=ucase : if mean input file, op mean output file, conv mean convert, ucase mean upper case
-also use to format pendrives
-
-dd if=inputFileName of=OutputFileName conv=ebcdic : convertng to european character encoding format
-
-use file * to check the format then we get  Non-ISO extended-ASCII text, with NEL line terminators
-
-
-##### man command : short for manual command
-man is the help repository for the command in unix
-man commandName : it will open all manual for the command
-
-
-#### bannder command
- banner "bye bye" : it will display 10 char in a line 
- banner bye bye it will splits the bye bye and shows it
-
-#### compress command
-compress -v fileName : -v is for shell to show the %age of compression
-
-## To decompress
-uncompress fileName.z
-
-### to see the compress file
-zcat fileName
-
-############################### Shell programming
+############################### Shell programming ###########################
 
 No multiline comment
 
 # To excute the script file 
 sh fileName.sh : 
 
-###################################################################################################################################################################################################
+# ----------------------
 
 # this is comment
 echo "Hello World"
